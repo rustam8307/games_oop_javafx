7ffd5729ddf9e541635f0f6faf4c7ed2798c4b8b
macbookair <rustam8307>
2021-01-10 23:17:08 +0300

    2. Сокобан. Тестовое задание. [#414598]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,36 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for (int diagIndx = 0; diagIndx < board.length; diagIndx++) {
+            if (board[diagIndx][diagIndx] == 1) {
+                rsl = checkColumn(board, diagIndx) || checkRow(board, diagIndx);
+                if (rsl) {
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean checkColumn(int[][] array, int column) {
+        boolean rsl = true;
+        for (int j = 0; j < array.length; j++) {
+            if (array[column][j] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean checkRow(int[][] array, int row) {
+        boolean rsl = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][row] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
